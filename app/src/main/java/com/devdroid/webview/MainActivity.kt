ab7c6ae45f5753361f2c66ed44b3ffcbdad64811
AhmadDroidDev <devdroid47@gmail.com>
2024-07-15 13:06:30 -0700

    first commit

---
+++ app/src/main/java/com/devdroid/webview/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.devdroid.webview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devdroid.webview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+//        supportActionBar?.hide()
+
+        binding.webView.loadUrl("https://hdmovies4u.rsvp/")
+        binding.webView.webViewClient = WebViewClient()
+
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check whether the key event is the Back button and if there's history.
+        if (keyCode == KeyEvent.KEYCODE_BACK && binding.webView.canGoBack()) {
+            binding.webView.goBack()
+            return true
+        }
+        // If it isn't the Back button or there isn't web page history, bubble up to
+        // the default system behavior. Probably exit the activity.
+        return super.onKeyDown(keyCode, event)
+    }
+}
